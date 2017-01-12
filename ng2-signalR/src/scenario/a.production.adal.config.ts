6c21f23b7c5f57a6fe0e33178c912d87ed40b472
unknown <EXK431@PO140027825.msnet.railb.be>
2017-01-12 16:34:42 +0100

    removes

--- ng2-signalR/src/scenario/a.production.adal.config.ts
+++
@@ -1,9 +0,0 @@
-import { AdalConfig } from '../adal.config';
-
-export const ATenantConfig = new AdalConfig('61bdbb45-4004-48e3-4444-e4f1740661c8', 'unittest.onmicrosoft.com', 'http://localhost/login', 'http://localhost/logout');
-
-export const ATenantUrl = 'https://login.microsoftonline.com/unittest.onmicrosoft.com/oauth2/authorize?response_type=id_token&' +
-    'client_id=61bdbb45-4004-48e3-4444-e4f1740661c8&redirect_uri=http%3A%2F%2Flocalhost&state=xxx&client-request-id=xxx&x-client-SKU=Js&x-client-Ver=1.0.0&nonce=xxx';
-
-
-
