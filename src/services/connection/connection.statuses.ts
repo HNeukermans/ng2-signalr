8fcf01f294bb7022374ab8a55c8b7be67487c113
Gittermans <hannes_neukermans@hotmail.com>
2017-06-13 19:57:54 +0200

    refactored ConnectionStatuses

--- src/services/connection/connection.statuses.ts
+++ src/services/connection/connection.statuses.ts
@@ -4,40 +4,25 @@ export class ConnectionStatuses {
 
     private static statuses: ConnectionStatus[] =
         [
-            new ConnectionStatus("starting"),
-            new ConnectionStatus("received"),
-            new ConnectionStatus("connectionSlow"),
-            new ConnectionStatus("reconnecting"),
-            new ConnectionStatus("reconnected"),
-            new ConnectionStatus("stateChanged"),
-            new ConnectionStatus("disconnected"),
+            new ConnectionStatus(0),
+            new ConnectionStatus(1),
+            new ConnectionStatus(2),
+            new ConnectionStatus(4)
         ];
 
-    public static get starting(): ConnectionStatus {
+    public static get connecting(): ConnectionStatus {
         return ConnectionStatuses.statuses[0];
     }
 
-    public static get received(): ConnectionStatus {
+    public static get connected(): ConnectionStatus {
         return ConnectionStatuses.statuses[1];
     }
 
-    public static get connectionSlow(): ConnectionStatus {
+    public static get reconnecting(): ConnectionStatus {
         return ConnectionStatuses.statuses[2];
     }
 
-    public static get reconnecting(): ConnectionStatus {
-        return ConnectionStatuses.statuses[3];
-    }
-
-    public static get reconnected(): ConnectionStatus {
-       return ConnectionStatuses.statuses[4];
-    }
-
-    public static get stateChanged(): ConnectionStatus {
-        return ConnectionStatuses.statuses[5];
-    }
-
     public static get disconnected(): ConnectionStatus {
-       return ConnectionStatuses.statuses[6];
+        return ConnectionStatuses.statuses[3];
     }
 }
