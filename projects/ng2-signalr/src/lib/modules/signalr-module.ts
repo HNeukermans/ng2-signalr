cab381de6a51da10310c8918f0039ff29f6f14f2
Henk Kelder <h.kelder@pxs.com>
2021-12-30 12:54:28 +0100

    version 12.0.2

--- projects/ng2-signalr/src/lib/modules/signalr-module.ts
+++ projects/ng2-signalr/src/lib/modules/signalr-module.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { NgModule, ModuleWithProviders, NgZone, InjectionToken } from '@angular/core';
 import { SignalR } from '../services/signalr';
 import { SignalRConfiguration } from '../services/signalr.configuration';
@@ -6,60 +7,55 @@ const SIGNALR_CONFIGURATION = new InjectionToken<SignalRConfiguration>('SIGNALR_
 
 export function createSignalr(configuration: SignalRConfiguration, zone: NgZone) {
 
-    const jConnectionFn = getJConnectionFn();
+  const jConnectionFn = getJConnectionFn();
 
-    return new SignalR(configuration, zone, jConnectionFn);
+  return new SignalR(configuration, zone, jConnectionFn);
 }
 
 function getJConnectionFn(): any {
-    const jQuery = getJquery();
-    const hubConnectionFn = jQuery.hubConnection;
-    if (hubConnectionFn == null) {
-        throw new Error('Signalr failed to initialize. Script \'jquery.signalR.js\' is missing. Please make sure to include \'jquery.signalR.js\' script.');
-    }
-    return hubConnectionFn;
+  const jQuery = getJquery();
+  const hubConnectionFn = jQuery.hubConnection;
+  if (hubConnectionFn == null) {
+    throw new Error('Signalr failed to initialize. Script \'jquery.signalR.js\' is missing. Please make sure to include \'jquery.signalR.js\' script.');
+  }
+  return hubConnectionFn;
 }
 
 function getJquery(): any {
-    const jQuery = (window as any).jQuery;
-    if (jQuery == null) {
-        throw new Error('Signalr failed to initialize. Script \'jquery.js\' is missing. Please make sure to include jquery script.');
-    }
-    return jQuery;
-}
-
-export function provideSignalr(): NgZone {
-  return new NgZone({});
+  const jQuery = (window as any).jQuery;
+  if (jQuery == null) {
+    throw new Error('Signalr failed to initialize. Script \'jquery.js\' is missing. Please make sure to include jquery script.');
+  }
+  return jQuery;
 }
 
 @NgModule({
-    providers: [{
-        provide: SignalR,
-        useValue: SignalR
-    }]
+  imports: [
+    CommonModule
+  ],
+  providers: [{
+    provide: SignalR,
+    useValue: SignalR
+  }]
 })
 export class SignalRModule {
-    public static forRoot(getSignalRConfiguration: () => void): ModuleWithProviders<SignalRModule> {
-        return {
-            ngModule: SignalRModule,
-            providers: [
-                {
-                    provide: SIGNALR_CONFIGURATION,
-                    useFactory: getSignalRConfiguration
-                },
-                {
-                    deps: [SIGNALR_CONFIGURATION, NgZone],
-                    provide: SignalR,
-                    useFactory: (createSignalr)
-                },
-                {
-                  provide: NgZone,
-                  useFactory: (provideSignalr)
-                }
-            ],
-        };
-    }
-    public static forChild(): ModuleWithProviders<SignalRModule> {
-        throw new Error('forChild method not implemented');
-    }
+  public static forRoot(getSignalRConfiguration: () => void): ModuleWithProviders<SignalRModule> {
+    return {
+      ngModule: SignalRModule,
+      providers: [
+        {
+          provide: SIGNALR_CONFIGURATION,
+          useFactory: getSignalRConfiguration
+        },
+        {
+          deps: [SIGNALR_CONFIGURATION, NgZone],
+          provide: SignalR,
+          useFactory: (createSignalr)
+        }
+      ],
+    };
+  }
+  public static forChild(): ModuleWithProviders<SignalRModule> {
+    throw new Error('forChild method not implemented');
+  }
 }
